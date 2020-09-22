db43902128dccda3460a59db67f7891532b7c1b6
Marvellous <fmarvelsk@gmail.com>
2020-09-22 16:23:08 +0100

    news-app

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@ var articles = document.querySelector('.articles');
 var publish = document.querySelector('#create-form');
 const loggedOutLinks = document.querySelectorAll('.logged-out');
 const loggedInLinks = document.querySelectorAll('.logged-in');
-
+var filename;
+var file = document.getElementById('filename')
 const setupUI = (user) => {
   if (user) {
     var acc = document.querySelector('.account-details')
@@ -25,83 +26,69 @@ var setupArticles = function(data){
     var keys = Object.keys(data)
     for(i=0; i< keys.length; i++){
         var k = keys[i]
-        var li = `<li class="flex-item"><h5>${data[k].title}</h5><img ${data[k].urlToImage}</li>`;
+        var image = data[k].urlToImage 
+        var li = `<li class="flex-item"><h5>${data[k].title}</h5><img src="${image}"></li>`;
+        
         dom+= li
     }
   articles.innerHTML = dom;  
 }   else{
     articles.innerHTML = '<div class=""> Welcome to the admin page Login</div>'
 }
+
 publish.addEventListener('submit', function(e){
   e.preventDefault();
   var key = [i].toString()
-
+  
+  var me = "";
+  if(file){
+    me = file.on('change', function(event){
+    filename = event.target.files[0];
+  var storageRef = firebase.storage().ref('/Image/' + file.name);
+  var uploadTask = storageRef.put(filename)
+  uploadTask.on('state_changed', function(snapshot){
+    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log('Upload is ' + progress + '% done');
+    switch (snapshot.state) {
+      case firebase.storage.TaskState.PAUSED:
+        console.log('Upload is paused');
+        break;
+      case firebase.storage.TaskState.RUNNING: 
+        console.log('Upload is running');
+        break;
+    }
+  }, function(error) {
+    console.log('error loading file')
+  }, function() {
+    uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+      console.log('File available at', downloadURL);
+    });
+  })
+  })
+  } 
   db.ref('articles').child('articles').child(key).update({
      author: publish['author'].value,
      content: publish['content'].value,
      publishedAt: publish['publishedAt'].value,
       title: publish['title'].value,
-      urlToImage: publish['image'].value,
-
+      
   }).then(function(){
-      console.log('Successfully written to Database')
-  })
-  .catch(function(err){
-      console.log('Error', err)
-  })
-  
+    console.log('Successfully written to Database')
+})
+.catch(function(err){
+    console.log('Error', err)
+})
 const modal = document.querySelector('#modal-create')
 M.Modal.getInstance(modal).close();
 publish.reset();
 })
 }
+
 document.addEventListener('DOMContentLoaded', function(){
     var modals = document.querySelectorAll('.modal');
     M.Modal.init(modals);
-    
     var items = document.querySelectorAll('collapsible');
     M.Collapsible.init(items);
-    
+  
+})
 
-}) 
-/**
-var data = document.getElementById('image').value
-var file = new Blob('me', {type:})
-var metadata = {
-    contentType: 'image/jpeg'
-  };
-var storageRef = storage.ref();
-  var uploadTask = storageRef.child('images/' + file.name).put(file, metadata);
-  
-  uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
-    function(snapshot) {
-     var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log('Upload is ' + progress + '% done');
-      switch (snapshot.state) {
-        case firebase.storage.TaskState.PAUSED: // or 'paused'
-          console.log('Upload is paused');
-          break;
-        case firebase.storage.TaskState.RUNNING: // or 'running'
-          console.log('Upload is running');
-          break;
-      }
-    }, function(error) {
-    switch (error.code) {
-      case 'storage/unauthorized':
-        // User doesn't have permission to access the object
-        break;
-  
-      case 'storage/canceled':
-        // User canceled the upload
-        break;
-  case 'storage/unknown':
-        // Unknown error occurred, inspect error.serverResponse
-        break;
-    }
-  }, function() {
-    // Upload completed successfully, now we can get the download URL
-    uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-      console.log('File available at', downloadURL);
-    });
-  });
-*/
